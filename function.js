acc0743b7ca49a5e2b6b244c52b136f92408b19a
pidorok228rus <118135432+farcis28800@users.noreply.github.com>
2024-05-29 19:14:16 +0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,33 @@
+let clicks = localStorage.getItem("coins") ? parseInt(localStorage.getItem("coins")) : 0;
+let boosts = localStorage.getItem("boosts") ? parseInt(localStorage.getItem("boosts")) : 1;
+let clickss = localStorage.getItem("clickss") ? parseInt(localStorage.getItem("clickss")) : 1;
+let price_boosts = localStorage.getItem("price_boosts") ? parseInt(localStorage.getItem("price_boosts")) : 10;
+let num_click = 1;
+
+function countClicks() {
+   let clicks_def = clickss;
+   clicks += clicks_def;
+   document.getElementById("clicks").innerHTML = clicks;
+   localStorage.setItem("coins", clicks);
+};
+
+function buyboostclick(){
+   if (clicks >= price_boosts) {
+     clicks -= price_boosts;
+     localStorage.setItem("coins", clicks);
+     clickss += num_click;
+     boosts += 1;
+     num_click *= 2;
+     price_boosts *= 4;
+     localStorage.setItem("boosts", boosts);
+     localStorage.setItem("clickss", clickss);
+     localStorage.setItem("price_boosts", price_boosts);
+     alert("Вы успешно улучшили кликер до " +  boosts)
+   } else {
+     alert("Недостаточно средств!")
+   }
+ }
+
+document.getElementById("boosts_lvl").innerHTML = "Уровень буста - " + boosts;
+document.getElementById("price_boost").innerHTML = "Цена буста - " +  price_boosts;
+document.getElementById("click_up").innerHTML = "Сколько кликов - " + clickss;
